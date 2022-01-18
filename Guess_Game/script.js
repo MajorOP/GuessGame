f03c9be2c0fffd8aca5f2fb39509dfb5c7310574
Umar <bcsf19a533@pucit.edu.pk>
2022-01-18 21:59:49 +0500

    initial commit

---
+++ Guess_Game/script.js
@@ -0,0 +1,35 @@
+let guessNumber = Math.ceil(Math.random() * 20);
+let message = document.querySelector(".message");
+let score = 20;
+let highscore = 0;
+document.querySelector(".check").addEventListener("click", function () {
+  let guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    message.textContent = "No Number!";
+  } else if (guessNumber === guess) {
+    message.textContent = "Congratulations, You Won!";
+    document.querySelector(".number").textContent = guessNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess > guessNumber) {
+    message.textContent = "Too High!";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < guessNumber) {
+    message.textContent = "Too Low!";
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  guessNumber = Math.ceil(Math.random() * 20);
+  score = 20;
+  highscore = 0;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".highscore").textContent = highscore;
+  message.textContent = "Start guessing...";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
